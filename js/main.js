72e6f83c2f5cc3526ad3a9844bb2eed6f55b56ca
SmejiaG <samuel.mejia@globant.com>
2020-03-01 16:27:38 -0500

    add services section

--- js/main.js
+++ js/main.js
@@ -93,4 +93,19 @@
     $(".mobile-nav, .mobile-nav-toggle").hide();
   }
 
+  var heroCarousel = $("#heroCarousel");
+  var heroCarouselIndicators = $("#hero-carousel-indicators");
+  heroCarousel.find(".carousel-inner").children(".carousel-item").each(function(index) {
+    (index === 0) ?
+    heroCarouselIndicators.append("<li data-target='#heroCarousel' data-slide-to='" + index + "' class='active'></li>"):
+      heroCarouselIndicators.append("<li data-target='#heroCarousel' data-slide-to='" + index + "'></li>");
+  });
+
+  heroCarousel.on('slid.bs.carousel', function(e) {
+    $(this).find('h2').addClass('animated fadeInDown');
+    $(this).find('p').addClass('animated fadeInUp');
+    $(this).find('.btn-get-started').addClass('animated fadeInUp');
+  });
+
+
 })(jQuery);
